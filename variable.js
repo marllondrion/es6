257bba3fd2de1b6e18443b4366fc62713b8c06c7
Marllon Drion <marllon@ares.com.au>
2021-04-10 16:42:23 +1000

    first commit

---
+++ variable.js
@@ -0,0 +1,14 @@
+//global
+var title = 'main title'
+
+//Assignment to constant variable
+const neverChange = 'Never'
+
+//block
+if(true){
+  let  title = 'different title'
+  console.log(title);
+//neverChange = 'ddd'
+}
+
+console.log(title);
